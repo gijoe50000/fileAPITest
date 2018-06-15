f269170963b3feffd8fee7bbd35694d1b59ca704
joeos <gijoe0000@gmail.com>
2018-06-15 12:17:12 +0100

    added stoppropagation method to prevent bubbling

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ var obj;
 
 this.addEventListener('dragstart', dstartfunc);
 
-main.addEventListener('drop', ddrop);
+main.addEventListener('drop', ddrop,);
 main.addEventListener('dragover', dover);
 
 locate.addEventListener('drop', ddrop);
@@ -22,30 +22,27 @@ h2.addEventListener('drop',ddrop)
 
 
 
-
-
 function dstartfunc(e){
+    e.stopPropagation();
     e.dataTransfer.setData('text/html', e.target.id);
 }
 
 
 function ddrop(e){
+    e.stopPropagation();
     obj=e.dataTransfer.getData('text/html');
-    
-    if(this.parentElement.id=="main"||this.parentElement.id=="container"){
     this.appendChild(document.getElementById(obj));
-    }
-    else
-    {
-        console.log("Parentid= "+this.parentElement.id);
-        this.child.appendChild(document.getElementById(obj));
-    }
+            console.log("this.id= "+this.id);
+ 
 
 }
 
 
 function dover(e){
     e.preventDefault();
-    // console.log(this);
-    if(this.parentElement.id!=""){ console.log("parent element: "+this.parentElement.id);}
+    e.stopPropagation();
+    console.log(this.id);
+    if(this.parentElement.id!=""){ 
+        // console.log(" element: "+this.id);
+    }
 }
